d711134e7f10cf79a5eac22e914c953ebb1cbded
Jeas0001 <jeas0001@edu.mercantec.dk>
2025-03-24 12:00:18 +0100

    staged

---
+++ backend/Api/Models/MQTTMessageReceive.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models
+{
+    public class MQTTMessageReceive
+    {
+        public double temperature { get; set; }
+
+        public string device_id { get; set; }
+
+        public int timestamp { get; set; }
+    }
+}
